eb99028a621d22c00956884558a4c79c91ecfcd2
aureliens53 <aureliensauvey@gmail.com>
2025-12-18 15:53:29 +0100

    comment

--- MonstreDePoche/models/Effects/EffectBurn.java
+++ MonstreDePoche/models/Effects/EffectBurn.java
@@ -5,6 +5,7 @@ public class EffectBurn extends Effect {
     public double burnDamage = 0.10; 
 
     public EffectBurn(String name, double rateOfEffect) {
+        // pour le pourcentage d'effectuer une brulure, on fait la moyenne entre le rateOfEffect passé en paramètre et un nombre aléatoire entre 10% et 90%
         super(name, (rateOfEffect + (0.10 + (Math.random() * (0.90 - 0.10))))/2);
     }
     
